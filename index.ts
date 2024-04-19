ee81997c4cd4c5c222ea5fdd25e0ffb5ec6574c9
mrbilal457897 <163662455+mrbilal457897@users.noreply.github.com>
2024-04-19 19:23:17 +0500

    word counter tool

---
+++ index.ts
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+const answer = await inquirer.prompt({
+    name: "sentence",
+    type: "input",
+    message: "Please enter your sentence",
+});
+
+const words = answer.sentence.trim().split(" ");
+
+console.log(words);
+
+
+console.log(`Your sentence word count is ${words.length}`);
